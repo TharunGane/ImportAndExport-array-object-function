c57e6bf5c7db6b364513a71b7ae24e5086bd422c
root <ganetharun1234@gmail.com>
2024-09-10 11:34:22 +0000

    import and export arr fun obj

---
+++ exportMultipleValues.mjs
@@ -0,0 +1,7 @@
+export const myArray = ["camel", 265, true, 5.6];
+export const bulb = { watt: 10, type: "LED" };
+export const multiplyByFour = (num) => {
+  if (num % 4 === 0) {
+    return num;
+  }
+};
---
+++ importMultipleValues.mjs
@@ -0,0 +1,5 @@
+import { myArray, bulb, multiplyByFour } from "./exportMultipleValues.mjs";
+
+console.log(multiplyByFour(24));
+console.log(myArray);
+console.log(bulb);
